a2e60a9a101129a15d2b1a5ec59127edb3774f93
nspp99b <chromepanthers@gmail.com>
2018-01-30 12:37:27 -0500

    defined components with synthetic event handling

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+
+  logFocus() {
+    console.log('Good!')
+  }
+
+  logDrift() {
+    console.log('Hey! Eyes on me!')
+  }
+
+  render() {
+    return (
+      <div>
+        <button onFocus={this.logFocus} onBlur={this.logDrift}></button>
+      </div>
+    )
+  }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,21 @@
 // Code Keypad Component Here
+import React from 'react';
+
+class Keypad extends React.Component {
+
+  logger() {
+    console.log('Entering password...')
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="password" onKeyUp={this.logger}> </input>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Keypad;
